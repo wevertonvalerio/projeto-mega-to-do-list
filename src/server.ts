c5e4ffa3e6bfedea51cdbe76ddbc41530a04c8f4
Weverton <weverton.valerio.silva@gmail.com>
2025-05-02 19:03:29 -0400

    inicio do projeto

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express = require('express');
+
+const PORT =  process.env.PORT || 3000;
+const HOST = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT);
+console.log(`Rodando: ${PORT}`);
