eef1a6f93e90fe70c2848053b48547c612a568cf
VitorHugoGarcia <vh.batistagarcia@gmail.com>
2025-05-28 09:47:46 -0400

    feature: Cadastro/Login Usuario

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import routerUsuario from "./routes/user-routes";
-import routerTasks from "./routes/task-routes";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,6 +7,5 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use("/usuario", routerUsuario);
-app.use("/tasks", routerTasks);
 
 export default app;
